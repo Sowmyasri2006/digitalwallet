7d6e9fdf2fa9f9797db3484869aa7bb973db960a
Sowmyasri2006 <sowmyasri849@gmail.com>
2025-09-27 09:47:06 +0530

    Update auth.js

--- ewallet-frontend/src/services/auth.js
+++ ewallet-frontend/src/services/auth.js
@@ -1,4 +1,6 @@
 import api from '../api/axios';
 
+const API_URL="http://54.163.30.213:9091/auth";
+
 export const loginUser = (credentials) => api.post('/auth/login', credentials);
 export const registerUser = (user) => api.post('/auth/register', user);
